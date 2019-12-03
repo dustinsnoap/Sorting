401b59d41101f2fe6b9dd079e8ef5aa1650c23fb
unknown <afrobacon@gmail.com>
2019-12-03 01:55:21 -0500

    merge sort in place

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -9,7 +9,6 @@ def merge( arrA, arrB ):
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort(arr):
-    print(arr)
     if len(arr) <= 1: return arr
 
     left_arr = merge_sort(arr[:len(arr)//2])
@@ -33,14 +32,37 @@ def merge_sort(arr):
 # STRETCH: implement an in-place merge sort algorithm
 def merge_in_place(arr, start, mid, end):
     # TO-DO
-
+    #NOPE
     return arr
 
-def merge_sort_in_place(arr, l, r): 
-    # TO-DO
+def merge_sort_in_place(array, left_start=None, right_end=None): 
+    #if there are no elements in this section of the array
+    if right_end - left_start == 0: return
+    
+    #set up pointers
+    if left_start == None: left_start = 0
+    if right_end == None: right_end = len(array) - 1
+    left_end = left_start + (right_end - left_start) // 2
+    right_start = left_end + 1
 
-    return arr
+    #sort left half of array section
+    merge_sort_in_place(array, left_start, left_end)
+    #sort right half of array section
+    merge_sort_in_place(array, right_start, right_end)
 
+    #while both array sections contain elements
+    while left_start <= left_end and right_start <= right_end:
+        #if left_start value is greater than right_start value
+        if array[left_start] > array[right_start]:
+            #remove right_start value then insert it at left_start
+            array.insert(left_start,array.pop(right_start))
+            #left section has a new element
+            #so push left_end and right_start ahead 1 index
+            right_start += 1
+            left_end += 1
+        left_start += 1
+
+    return array
 
 # STRETCH: implement the Timsort function below
 # hint: check out https://github.com/python/cpython/blob/master/Objects/listsort.txt
